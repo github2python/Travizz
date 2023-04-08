5f97f7d07e5ed08985d1b8efaa5ecf4b57de9e35
coding@enthusiast <72038689+github2python@users.noreply.github.com>
2023-04-08 14:59:41 +0530

    Add files via upload

---
+++ config/keys_dev.js
@@ -0,0 +1,8 @@
+module.exports = {
+  mongoURI:
+    "mongodb://localhost:27017",
+  secretOrKey: "VJ6I1687Ip", // jsonwebtoken key
+  // mapApi: "GtJj838k2uUMChaJbStq3F7qM0WobjXf",
+  MAP_KEY:"pk.eyJ1IjoiZHQxMDI1IiwiYSI6ImNsZzI0MzZ2dTAxdWgzZm1wZndxZXliazUifQ.SbBELo0bEaMCVkSEkfkX_g",
+  YELP_KEY:""
+};
